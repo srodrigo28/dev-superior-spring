81960a57114a67aea260b306fddd6b011058cb8a
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-15 10:34:14 -0300

    Criando Clientes

--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO tb_worker(name, daily_Income) VALUES ('Bob', 200.0);
 INSERT INTO tb_worker(name, daily_Income) VALUES ('Maria', 300.0);
 INSERT INTO tb_worker(name, daily_Income) VALUES ('Alex', 250.0);
+
+
+INSERT INTO tb_cliente(nome, telefone, cpf) VALUES('ana olivera', '(62) 9999-8888', '010.999.888-99');
+INSERT INTO tb_cliente(nome, telefone, cpf) VALUES('edson emanoel', '(84) 85335-0753', '645.616.930-50');
+INSERT INTO tb_cliente(nome, telefone, cpf) VALUES('valentina sousa', '(66) 88700-1575', '167.913.900-26');
