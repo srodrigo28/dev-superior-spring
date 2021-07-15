be06d132d05f46d79c960b22bc58332d424752f4
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-07-15 10:52:21 -0300

    Criando Produto

--- hr-worker/src/main/resources/data.sql
+++ hr-worker/src/main/resources/data.sql
@@ -6,3 +6,7 @@ INSERT INTO tb_worker(name, daily_Income) VALUES ('Alex', 250.0);
 INSERT INTO tb_cliente(nome, telefone, cpf) VALUES('ana olivera', '(62) 9999-8888', '010.999.888-99');
 INSERT INTO tb_cliente(nome, telefone, cpf) VALUES('edson emanoel', '(84) 85335-0753', '645.616.930-50');
 INSERT INTO tb_cliente(nome, telefone, cpf) VALUES('valentina sousa', '(66) 88700-1575', '167.913.900-26');
+
+INSERT INTO TB_PRODUTO(produto, DESCRICAO, Quantidade, preco) VALUES('PS1', 'PlayStation 1, game do ano 2000', 2, 209.1 );
+INSERT INTO TB_PRODUTO(produto, DESCRICAO, Quantidade, preco) VALUES('PS2', 'PlayStation 2, game do ano 2010', 5, 509.1 );
+INSERT INTO TB_PRODUTO(produto, DESCRICAO, Quantidade, preco) VALUES('PS3', 'PlayStation 3, game do ano 2008', 10, 1209.1 );
