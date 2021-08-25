94486509ff3a7d52b22ff45b3081241688e3daf4
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-08-24 23:21:03 -0300

    spring step 2

---
+++ book/src/main/java/br/com/book/entity/Author.java
@@ -0,0 +1,40 @@
+package br.com.book.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "author")
+@Entity(name = "author")
+public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	private String name;
+	
+	public Author(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
